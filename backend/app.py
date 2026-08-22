77ba867af853a471f06b580e60e3ec862f4655ad
wunptiris <wunptiris@gmail.com>
2026-08-22 17:07:43 -0500

    Add backend/app.py

---
+++ backend/app.py
@@ -0,0 +1,70 @@
+
+# Import necessary libraries
+import numpy as np
+import joblib  # For loading the serialized model
+import pandas as pd  # For data manipulation
+import streamlit as st  # For creating the web app
+from flask import Flask, request, jsonify  # For creating the Flask API
+
+# Initialize Flask app with a name
+superkart_api = Flask("SuperKart Sales Prediction") #Complete the code to define the name of the app
+
+# Load the trained churn prediction model
+model = joblib.load(saved_model_path) #Complete the code to define the location of the serialized model
+
+# Define a route for the home page
+@superkart_api.get('/')
+def home():
+    """
+    This function handles GET requests to the root URL ('/') of the API.
+    It returns a simple welcome message.
+    """
+    return "Welcome to the SuperKart Sales Prediction API!" #Complete the code to define a welcome message
+
+# Define an endpoint to predict churn for a single customer
+@superkart_api.post('/v1/predict')
+def predict_sales():
+    # Get JSON data from the request
+    data = request.get_json()
+
+    # Extract relevant customer features from the input data. The order of the column names matters.
+    sample = {
+        'Product_Weight': data['Product_Weight'],
+        'Product_Sugar_Content': data['Product_Sugar_Content'],
+        'Product_Allocated_Area': data['Product_Allocated_Area'],
+        'Product_MRP': data['Product_MRP'],
+        'Store_Size': data['Store_Size'],
+        'Store_Location_City_Type': data['Store_Location_City_Type'],
+        'Store_Type': data['Store_Type'],
+        'Product_Id_char': data['Product_Id_char'],
+        'Store_Age_Years': data['Store_Age_Years'],
+        'Product_Type_Category': data['Product_Type_Category']
+    }
+
+    # Convert the extracted data into a DataFrame
+    input_data = pd.DataFrame([sample])
+
+    # Make a churn prediction using the trained model
+    prediction = model.predict(input_data).tolist()[0]
+
+    # Return the prediction as a JSON response
+    return jsonify({'Sales': prediction})
+
+# Define an endpoint for batch prediction (POST request)
+@superkart_api.post('/v1/batch_predict')
+def batch_predict():
+    # Get JSON data from the request
+    data = request.get_json()
+    
+    # Convert the list of dictionaries to a DataFrame
+    input_data = pd.DataFrame(data)
+
+    # Make batch predictions using the trained model
+    predictions = model.predict(input_data).tolist()
+
+    # Return the predictions as a JSON response
+    return jsonify({'Sales_Predictions': predictions})
+
+# Run the Flask app in debug mode
+if __name__ == '__main__':
+    superkart_api.run(debug=True)
